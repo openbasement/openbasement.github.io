d03d16a5c6666a0b4bc7f3255fb8460e2e32f12d
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2016-09-17 17:05:35 +0200

    Side bar demo

--- i18n/locales/en-US.js
+++ i18n/locales/en-US.js
@@ -1,21 +1,49 @@
 module.exports = {
+  // layout
+
   'title': 'OpenBasement',
   'subtitle': 'anti-social no-network',
   'footer': '¬©2016 OpenBasement team and (imagined) friends',
+
+  // main menu
 
   'Profile': 'Profile',
   'Friends': 'Friends',
   'Settings': 'Settings',
 
+  // side bar
+
   'Notifications': 'Notifications',
   'Events': 'Events',
   'Interactions': 'Interactions',
+
+  // side bar empty
 
   'no-notifications': 'no new notifications',
   'no-events': 'no events',
   'no-interactions': 'no interactions',
 
+  // journal buttons
+
   'meeting': 'meeting',
   'mood': 'mood',
-  'note': 'note'
+  'note': 'note',
+
+  // events
+
+  'entry-1': 'Congrats! First entry!',
+  'entry-2': 'And another one!',
+  'entry-5': 'I see that you get it',
+  'entry-10': 'You like spamming, don\'t you?',
+  'entry-20': 'Lot of messages',
+  'entry-50': 'So many messages to yourself - you are sooo lonely',
+  'entry-100': '100 messages that no one will ever read',
+  'entry-200': '200 messages to nobody - would you consider a bottle?',
+  'entry-500': '500 posts. You have some serious issues there',
+  'entry-1000': 'Ok, I shut up. It apparently turns you on.',
+
+  // interactions
+
+  'last-meeting': 'Last meeting: {time}',
+  'meetings-total': 'Total meetings: {total}'
 }
