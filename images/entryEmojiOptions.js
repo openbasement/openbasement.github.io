04ce7f2c191f686c2cbf7f72df690fc35017fabb
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2016-10-26 16:14:28 +0200

    Refactor and fixes

---
+++ images/entryEmojiOptions.js
@@ -0,0 +1,13 @@
+import emojioneOptions from './emojioneOptions';
+
+const entryEmojiOptions = {
+  ...emojioneOptions,
+  styles: {
+    ...emojioneOptions.styles,
+    height: '8.5pt',
+    width: '8.5pt',
+    margin: '0'
+  }
+};
+
+export default entryEmojiOptions;
---
+++ images/menuEmojiOptions.js
@@ -0,0 +1,14 @@
+import emojioneOptions from './emojioneOptions';
+
+const menuEmoijiOptions = {
+  ...emojioneOptions,
+  styles: {
+    ...emojioneOptions.styles,
+    height: '12pt',
+    width: '12pt',
+    margin: '0',
+    marginBottom: '-3.5pt'
+  }
+};
+
+export default menuEmoijiOptions;
